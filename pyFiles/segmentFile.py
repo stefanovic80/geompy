1fc724bfba13e2f0f3060c8bb33683c2e2b6b56c
stefanovic80 <stefanovicgh@gmail.com>
2023-08-16 14:18:03 +0200

    x limits of segments now are working!

--- pyFiles/segmentFile.py
+++ pyFiles/segmentFile.py
@@ -5,19 +5,21 @@ plt.ion()
 
 from .plotSettFile import plotSett
 
-
-
+#names = globals().keys()
 
 class segment(plotSett):
     def __init__(self, xmax = 10, xmin = -10):
 
         super().__init__()
-        self.angCoeff = np.random.randint(-20, 20)
+        self.angCoeff = np.random.randint(xmin, xmax)
         self.intercept = np.random.randint(xmin, xmax)
+        self.xmin = xmin
+        self.xmax = xmax
         self.idxMin = None
         self.idxMax = None
         self.segment = None
         self.data = None
+        self.name = globals().keys()
 
     def remove(self):
         try:
@@ -25,17 +27,13 @@ class segment(plotSett):
         except:
             pass
 
-    def plot(self, color = 'b'):
+    def plot(self, color = 'b', name = 'label'):
         
         self.remove()
-        #try:
-        #    self.segment.remove()
-        #except:
-        #    pass
         
-        #idxMin = np.where( obj.x >= xMin)[0][0]
-        #idxMax = np.where( obj.x >= xMax)[0][0]
-        #x = obj.x[idxMin: idxMax] # a local copy of x values
-        self.data = self.angCoeff*self.x + self.intercept
-        self.segment, = self.ax.plot(self.x, self.data, linewidth=2, color = color)
-
+        idxMin = np.where( self.x >= self.xmin)[0][0]
+        idxMax = np.where( self.x >= self.xmax)[0][0]
+        x = self.x[idxMin: idxMax] # a local copy of x values
+        self.data = self.angCoeff*x + self.intercept
+        self.segment, = self.ax.plot(x, self.data, linewidth=2, color = color, label = name)
+        self.ax.legend()
