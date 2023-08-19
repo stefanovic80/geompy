e0714983d8861bd362824fcf6c17ec6dcc3eba48
stefanovic80 <stefanovicgh@gmail.com>
2023-08-19 22:24:03 +0200

    __str__ a bit improved

--- pyFiles/circumferenceFile.py
+++ pyFiles/circumferenceFile.py
@@ -65,7 +65,8 @@ class circumference(plotSett):
 
     def __str__(self):
         attributes = (
-            f"Attributes:\n"
+            f"\033[93mType:\033[0m circumference\n"
+            f"\nAttributes:\n"
             f"\033[93mcenter:\033[0m {self.center[0]} {self.center[1]}\n"
             f"\033[93mradius:\033[0m {self.radius}\n"
             f"\033[93mx:\033[0m {self.x}\n"
