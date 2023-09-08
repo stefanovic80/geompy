eb9df67176bd1ec80ab1d4bde422a4589f18a3f9
stefanovic80 <stefanovicgh@gmail.com>
2023-09-08 22:43:32 +0200

    .pointFile.py modified so that point class can receive coords as argument. Bug with random numbers to be fixed!

--- pyFiles/pointFile.py
+++ pyFiles/pointFile.py
@@ -1,8 +1,6 @@
-# pointFile.py
-from . import plt, np, random, time
+#pointFile.py
+from . import plt, np, random
 from . import xmin, xmax, steps, linewidth
-
-#matplotlib.plot has to be imported in all pyFiles modules
 
 plt.ion()
 
@@ -12,61 +10,36 @@ from . import xmin, xmax, steps, linewidth
 x = random.uniform(xmin, xmax)
 y = random.uniform(xmin, xmax)
 
-
 class point(plotSett):
-    
-    def __init__(self, pickFrom = None, x = x, y = y, xmin = -20, xmax = 20, steps = 500, linewidth = 2):
-        super().__init__( xmin, xmax, steps, linewidth )
-
+    def __init__(self, pickFrom = None, x = x, y = y, xmin = -25, xmax = 25, steps = 500, linewidth = 2):
+        super().__init__( xmin, xmax, steps, linewidth)
         
-        p = (isinstance(pickFrom, int) or isinstance(pickFrom, float) or pickFrom == None)
-        print(p)
-        #if p == True:
-        #    x = pickFrom
-        #    y = x
-        self.coords = [x, y]
-        #else: 
-        try:
-            self.pickFrom = pickFrom.data
-        except:
-            self.pickFrom = pickFrom
         
-        self.lines = None
-        self.color = random.choice(self.colors)
-        self.name = None
+        #self.pickFrom = pickFrom
 
-
-        self.j = 0
-        self.text = None
-
-        #it works in case a geometrical locus is selected (.pickFrom) 
-        try:
-            self.randomPoint()
-        except:
+        if isinstance(pickFrom, int) or isinstance(pickFrom, float):# or pickFrom is None:
+            x = pickFrom
+            y = x
+        elif pickFrom is None:
             pass
+        else:
+            self.pickFrom = pickFrom
+            self.randomPoint()
+            #self.draw()
 
 
-    """
-    @property
-    def _name(self):
-        return self.name
+        self.coords = [x, y]
+        self.color = random.choice(self.colors)
+        self.j = 0
 
-    @_name.setter
-    def _name(self, new_name):
-        if new_name != self.name:
-            self.label()
-        self.name = new_name
-
-    #def _on_name_change(self, new_name):
-    #    self.label()
-    """
-
+        self.lines = None
+        self.text = None
+        self.name = None
 
     def draw(self, name = None):
         self.__del__()
 
         line = self.ax.scatter( self.coords[0], self.coords[1], color = self.color, linewidth = self.linewidth)
-
         self.lines = []
         self.lines.append(line)
 
@@ -79,19 +52,23 @@ class point(plotSett):
         else:
             print("\nrun .draw one more time to highlight coordinates\n")
         
+        
+
         try:
             self.name = name
             self.label()
         except:
             pass
 
+        self.j += 1
 
-        self.j+=1
+
     
-    #it works in case a geometrical locus is selected (.pickFrom)
+
     def randomPoint(self):
         idx = np.random.randint(0, len(self.pickFrom[0]) )
         self.coords = [ self.pickFrom[0][idx]  , self.pickFrom[1][idx]  ]
+
 
 
     def click(self):
@@ -105,11 +82,14 @@ class point(plotSett):
         shift = (self.xmax - self.xmin)/40
         self.text = self.ax.text(self.coords[0] + shift, self.coords[1] + shift, self.name, fontsize = 12, color = self.color, ha="center", va="center")
 
+
+
+
     def __str__(self):
 
         super().__str__()
-        
-        
+
+
         attributes = (
             f"\033[93mClass type:\033[0m point\n"
             f"\nAttributes:\n"
@@ -126,5 +106,4 @@ class point(plotSett):
         )
 
         return attributes + methods + self.plotSettings
-    
-    #a = globals().keys(), b = list(a)
+
