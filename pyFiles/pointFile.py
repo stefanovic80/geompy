.click() bug fixed by removing .lims() from .__del__(), .rotation method added on point class

pyFiles/pointFile.py
@@ -57,7 +57,8 @@ class point(plotSett):
         self.lines = None
         self.tex = None
         self.name = None
-        
+        #self.rotation = False
+        #self.data = self.coords
     def draw(self, name = None):
         self.__del__()
 
@@ -125,8 +126,7 @@ class point(plotSett):
             self.coords = [ b[0][0], b[0][1] ]
             self.draw(name)
             
-            self.ax.set_xlim( self.xmin, self.xmax  )
-            self.ax.set_ylim( self.xmin, self.xmax  )
+            self.lims()
         else: 
             a = plt.ginput()
             
@@ -136,6 +136,13 @@ class point(plotSett):
             self.ax.set_ylim( a[0][1] - deltaZoom, a[0][1] + deltaZoom)
             print("\nrun .click('label') one more time to select point coordinates\n")
         self.k += 1
+
+    def rotation(self, locus = None, angle = 0):
+        locus.rotation = True
+        a1 = (locus.data[0] - self.coords[0])*np.sin(-angle)
+        locus.data[0] = (locus.data[0] - self.coords[0]  )*np.cos(-angle) - (locus.data[1] - self.coords[1]  )*np.sin(-angle) + self.coords[0]
+
+        locus.data[1] = a1 + ( locus.data[1] - self.coords[1])*np.cos(-angle) + self.coords[1]
 
     def __str__(self):
 

pyFiles/segmentFile.py
@@ -43,7 +43,7 @@ class segment(plotSett):
         self.angCoeff = None #np.tan(angle)
         self.intercept = None
         self.name = None
-        
+        self.rotation = False
         self.cut = False
 
     def calc1(self): #calculate equation from angCoeff and intercept
@@ -123,19 +123,20 @@ class segment(plotSett):
 
     def chooseCalc(self):
         self.__del__()
-        try:
-            self.calc2()#two points
-        except:
+        if self.rotation == False:
             try:
-                self.calc1() #angCoeff, intercept
+                self.calc2()#two points
             except:
                 try:
-                    self.calc3() # 1point, angCoeff
+                    self.calc1() #angCoeff, intercept
                 except:
                     try:
-                        self.calc4()# 1point, intercept
+                        self.calc3() # 1point, angCoeff
                     except:
-                        pass
+                        try:
+                            self.calc4()# 1point, intercept
+                        except:
+                            pass
 
 
     def draw(self, name = None, cut = False ):