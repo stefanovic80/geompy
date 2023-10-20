b9ff45aa19480e5e12eb3e170835811ecb4058c5
stefanovic80 <stefanovicgh@gmail.com>
2023-10-20 18:04:16 +0200

    .calc() in circumference modified so that it starts calculating from the last one instead of the first one!

--- pyFiles/circumferenceFile.py
+++ pyFiles/circumferenceFile.py
@@ -37,28 +37,22 @@ class circumference(plotSett):
         #------------------------------------------------------
 
 
-
+    #circumference equation calculation from center coordinates and radius
     def calc(self, name = None):
-        #self.__del__()
+        left = self.center.coords[0] - self.radius
+        right = self.center.coords[0] + self.radius
         
-        circ = np.sqrt( self.radius**2 - (self.x- self.center.coords[0])**2)#circumference equation      
-
-        #it removes not a number terms due to root of negative values
-        idx1 = np.argmax(~np.isnan(circ))
-        idx2 = len(circ) - np.argmax(np.flip(~np.isnan(circ)))
-        #x values for the graph upper side
-        self.data = [self.x[idx1:idx2]]
-        circ = circ[idx1:idx2]
         
-        #y values as second column of self.data matrix
-        self.data = self.data + [ np.append( self.center.coords[1] + circ, self.center.coords[1] - circ[::-1] ) ]
+        xvalues = self.x[ (self.x >= left) & (self.x <= right)]
+         
+        self.data = [ np.concatenate( [ np.flip(xvalues), xvalues  ] ), np.zeros(2*len(xvalues) ) ]
 
-        #x values for the graph of the lower side
-        self.data[0] = np.append( self.data[0], self.data[0][::-1])
-
-        self.data[0] = np.append( self.data[0], self.data[0][0] )
-        self.data[1] = np.append( self.data[1], self.data[1][0] )
-
+        lastValuex = np.array( [ xvalues[-1] ] )
+        #to be fixed as last value is still missing
+        circ = np.sqrt( ( self.radius  )**2 - ( xvalues - self.center.coords[0]  )**2  )
+        self.data[1] = self.data[1] + np.concatenate( [ self.center.coords[1] + circ, self.center.coords[1] - circ  ] )
+        
+        
     def calc2(self, name = None):
         x0 = self.point[0].coords[0]
         x1 = self.point[1].coords[0]
@@ -84,8 +78,24 @@ class circumference(plotSett):
 
         self.radius = np.sqrt( ( x0 - x1  )**2 + ( x0 - x1 )**2  )
         self.calc()
+    
+    def chooseCalc(self):
+        self.__del__()
+        calculation_functions = [self.calc, self.calc2, self.calc3]
+
+        for calc_function in calculation_functions:
+            if self.rotate == False:
+                try:
+                    calc_function()
+                except:
+                    pass
+
 
     def draw(self, name = None):
+        
+        self.chooseCalc()
+
+        """
         self.__del__()
         if self.rotate == False:
             try:
@@ -98,6 +108,7 @@ class circumference(plotSett):
                         self.calc3()
                     except:
                         pass
+        """
 
         line1, = self.ax.plot(self.data[0], self.data[1], color = self.color, label = self.name, linewidth = self.linewidth)
         #self.ax.legend()
