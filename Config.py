71c0a657569e6d92e0f69ac0183c737fe75f69b2
stefanovic80 <stefanovicgh@gmail.com>
2023-11-25 23:09:51 +0100

    useless Config.py file removed, Settings.py set at xmin = -10 and xmax = 10

--- Config.py
+++
@@ -1,5 +0,0 @@
-class config():
-    linewidth = 1
-    steps = 1000
-    xmin = -12
-    xmax = 13
--- pyFiles/Settings.py
+++ pyFiles/Settings.py
@@ -1,6 +1,5 @@
 class settings():
     linewidth = 1
     steps = 1000
-    xmin = -12
-    xmax = 13
-    steps = 1000
+    xmin = -10
+    xmax = 10
