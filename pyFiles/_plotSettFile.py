65f45bf240f004daa99a4ac86efd845d72512763
stefanovic80 <stefanovicgh@gmail.com>
2023-12-01 18:21:58 +0100

    adding .a, .b and .c parameters into ellipse class!

--- pyFiles/_plotSettFile.py
+++ pyFiles/_plotSettFile.py
@@ -6,7 +6,7 @@ from .Settings import settings#xmin, xmax, linewidth, steps
 plt.ion()
 
 plt.rcParams [ 'axes.labelsize' ] = 18
-plt.rcParams [ 'lines.linewidth' ] = 2
+#plt.rcParams [ 'lines.linewidth' ] = 2
 plt.rcParams [ 'figure.figsize' ] = ( 9 , 9)
 plt.rcParams [ 'font.size' ] = 10
 plt.rcParams [ 'font.weight'] = 'bold'
