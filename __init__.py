d68c97488d4bc19e9d2197c4815617fc0d8e3e4f
stefanovic80 <stefanovicgh@gmail.com>
2023-12-02 12:00:14 +0100

    __init__.py file aded into main folder!

---
+++ __init__.py
@@ -0,0 +1,18 @@
+#%run main.py
+
+# Import functions and classes from your package
+from .pyFiles._plotSettFile import plotSett
+from .pyFiles.pointFile import point
+from .pyFiles.circumferenceFile import circumference
+from .pyFiles.ellipseFile import ellipse
+from .pyFiles.lineFile import line
+from .pyFiles.parabolaFile import parabola
+#from pyFiles.config import xmin, xmax
+
+#from .pyModules2gen.triangleFile import triangle
+#from .pyModules2gen.angleFile import angle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import os
