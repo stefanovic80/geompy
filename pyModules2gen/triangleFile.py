e83fef6eac10d58eb2b68d5fc6c87a1e1ac1bee7
stefanovic80 <stefanovicgh@gmail.com>
2024-01-23 21:04:53 +0100

    rotation works on triangle class, but labels do not!

--- pyModules2gen/triangleFile.py
+++ pyModules2gen/triangleFile.py
@@ -29,7 +29,6 @@ class triangle(dataExplore):
 
         if draw == True:
             self.draw()
-            #self.lines[0].remove()
 
     @property
     def labels(self):
@@ -55,8 +54,9 @@ class triangle(dataExplore):
                 l = k%3
                 self.vertices[l].__del__()
 
-
         self.j+=1
+
+
 
     def __del__(self):
         super().__del__()
@@ -81,41 +81,10 @@ class triangle(dataExplore):
                     pass
 
 
-
-    def calc(self):
-        
-        j = 0
+    def calc1(self):
         for l in range(2):
             self.data[l] = np.array([])
-        for side in self.sides:
-            k = (j+1)%3
-            #defines a side passing through vertices j and k
-            side.points = self.vertices[j]
-            side.points = self.vertices[k]
-            side.color = self._color
-            
-            lims = [ self.vertices[j].x[0], self.vertices[k].x[0] ]
-            lims.sort()
-            
-            #cut Off triangle sides
-            for l in range(2):
-                side.x = lims[l]
-                side.cutOff
-            
-            #side.lines[0].remove()
 
-            #append each one, x and y respectively of triangle sides data
-            for l in range(2):
-                #self.data[l] = np.append(self.data[l], side.data[l])
-                self.data[l] = np.concatenate((self.vertices[j].data[l], self.data[l], side.data[l], self.vertices[k].data[l]))
-
-            j +=1
-        
-        for j in range(3):
-            self.side[j].lines[j].remove()
-
-
-    def calc1(self):
         for k in range(2):
             for j in range(4):
                 l = j%3
