b94c78ee523f48cd6fadd6d41d721ea125a8c32c
stefanovic80 <stefanovicgh@gmail.com>
2024-11-06 22:32:35 +0100

    all calculation method added

--- pyFiles/parabolaFile.py
+++ pyFiles/parabolaFile.py
@@ -163,11 +163,16 @@ class parabola(dataExplore):
     #a, b and c (to be modified!)
     def calc1(self, name = None):
 
+        self.data = [ self._x ]
+        self.data = self.data + [self._a*self._x**2 + self._b*self._x + self._c ]
+
+
+        """
         Delta = self._b**2 - 4*self._a*self._c
         self._vertex = point( -self._b/(2*self._a)  , -Delta/(4*self._a),  draw = False )
         #self.concavity = self._a
         self.calc()
-
+        """
 
 
     # calculate from three points the parabola passing through (to be fixed!)
@@ -230,6 +235,111 @@ class parabola(dataExplore):
         self.calc1()
 
 
+
+
+    #point0, point1 and a (self._a)
+    def calc6(self, name = None):
+        u = self.getPoint()
+        point0 = next(u)
+        point1 = next(u)
+        x0, y0 = point0.coords[0], point0.coords[1]
+        x1, y1 = point1.coords[0], point1.coords[1]
+
+        A = np.matrix( [ [ x0, 1 ], [ x1, 1  ] ] )
+        Ainv = np.linalg.inv(A)
+        y = np.array( [y0 - self._a*x0**2, y1 - self._a*x1**2 ] )
+        parabParams = np.dot(Ainv, y)
+
+        self._b = parabParams[0, 0]
+        self._c = parabParams[0, 1]
+
+        self.calc1()
+
+
+
+    #point0, point1 and b (self._b)
+    def calc7(self, name = None):
+        u = self.getPoint()
+        point0 = next(u)
+        point1 = next(u)
+        x0, y0 = point0.coords[0], point0.coords[1]
+        x1, y1 = point1.coords[0], point1.coords[1]
+
+        A = np.matrix( [ [ x0**2, 1 ], [ x1**2, 1  ] ] )
+        Ainv = np.linalg.inv(A)
+        y = np.array( [y0 - self._b*x0, y1 - self._b*x1 ] )
+        parabParams = np.dot(Ainv, y)
+
+        self._a = parabParams[0, 0]
+        self._c = parabParams[0, 1]
+
+        self.calc1()
+
+
+    #point0, a (self._a) and b (self._b)
+    def calc8(self, name = None):
+        u = self.getPoint()
+        point0 = next(u)
+        x0, y0 = point0.coords[0], point0.coords[1]
+        self._c = y0 - self._a*x0**2 - self._b*x0
+
+        self.calc1()
+
+
+    #point0, a (self._a) and c (self._c)
+    def calc9(self, name = None):
+        u = self.getPoint()
+        point0 = next(u)
+        x0, y0 = point0.coords[0], point0.coords[1]
+        self._b = (y0 - self._a*x0**2 - self._c)/x0
+
+        self.calc1()
+
+
+    #point0, b (self._b) and c (self._c)
+    def calc10(self, name = None):
+        u = self.getPoint()
+        point0 = next(u)
+        x0, y0 = point0.coords[0], point0.coords[1]
+        self._a = (y0 - self._b*x0 - self._c)/x0**2
+
+        self.calc1()
+
+
+    #vertex, b (self._b)
+    def calc11(self, name = None):
+        
+        xv, yv = self.vertex.coords[0], self.vertex.coords[1]
+
+        self._a = - self._b/(2*self._a)
+        self._c = yv + self._b**2/(4*self._a)
+
+        self.calc1()
+
+
+    #vertex, c (self._c)
+    def calc12(self, name = None):
+
+        xv, yv = self.vertex.coords[0], self.vertex.coords[1]
+
+        self._a = (self._c - yv)/xv
+        self._b = -2*xv*(self._c - yv)
+
+        self.calc1()
+
+
+    #vertex, a (self._a)
+    def calc13(self, name = None):
+
+        xv, yv = self.vertex.coords[0], self.vertex.coords[1]
+
+        self._b = -2*self._a*xv
+        self._c = yv + self._b**2/(4*self._a)
+
+        self.calc1()
+
+
+
     """
     def chooseCalc(self):
         self.__del__()
@@ -270,11 +380,52 @@ class parabola(dataExplore):
             self.onlyDraw()
         
         #5) c (self._c), point0, point1
-        #elif 'c' in self.params.keys() and any(isinstance(key, str) and sum(1 for key in self.params.keys() if key.startswith("point")) == 2:
         elif 'c' in self.params.keys() and ( sum(1 for key in self.params.keys() if isinstance(key, str) and key.startswith("point")) == 2):
             self.calc5()
             self.onlyDraw()
         
+        #6) a (self._a), point0, point1
+        elif 'a' in self.params.keys() and ( sum(1 for key in self.params.keys() if isinstance(key, str) and key.startswith("point")) == 2):
+            self.calc6()
+            self.onlyDraw()
+
+        #7) b (self._b), point0, point1
+        elif 'b' in self.params.keys() and ( sum(1 for key in self.params.keys() if isinstance(key, str) and key.startswith("point")) == 2):
+            self.calc7()
+            self.onlyDraw()
+
+        #8) a (self._a), b (self._b), point0
+        elif 'a' in self.params.keys() and 'b' in self.params.keys() and any(isinstance(key, str) and key.startswith(prefix) for key in self.params.keys() ): 
+            self.calc8()
+            self.onlyDraw()
+
+        #9) a (self._a), c (self._c), point0
+        elif 'a' in self.params.keys() and 'c' in self.params.keys() and any(isinstance(key, str) and key.startswith(prefix) for key in self.params.keys() ):
+            self.calc9()
+            self.onlyDraw()
+
+        #10) b (self._b), c (self._c), point0
+        elif 'b' in self.params.keys() and 'c' in self.params.keys() and any(isinstance(key, str) and key.startswith(prefix) for key in self.params.keys() ):
+            self.calc10()
+            self.onlyDraw()
+
+        #11) vertex, b (self._b)
+        elif 'vertex' in self.params.keys() and 'b' in self.params.keys():
+            self.calc11()
+            self.onlyDraw()
+
+        #12) vertex, c (self._c)
+        elif 'vertex' in self.params.keys() and 'c' in self.params.keys():
+            self.calc12()
+            self.onlyDraw()
+
+        #13) vertex, a (self._a)
+        elif 'vertex' in self.params.keys() and 'a' in self.params.keys():
+            self.calc13()
+            self.onlyDraw()
+
+
+
         else:
             pass
 
