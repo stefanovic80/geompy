0938a4db2537fe055cb45b78a976dd038f06426a
stefanovic80 <stefanovicgh@gmail.com>
2025-01-02 22:57:11 +0100

    no weakref added!

--- pyFiles/keys/line_listOfKeys.py
+++ pyFiles/keys/line_listOfKeys.py
@@ -1,12 +1,12 @@
 from ..line.lineCalcFile import lineCalc
-from ..line.lineCalcFile import m_po, m_q, po_po, po_q
+#from ..line.lineCalcFile import m_po, m_q, po_po, po_q
 
 class method(lineCalc):
     def __init__(self):
         super().__init__()
         self.draws = {
-            ('m', 'po'): m_po,
-            ('m', 'q'): m_q,
-            ('po', 'po'): po_po,
-            ('po', 'q'): po_q
+            ('m', 'po'): self.calc_m_po,
+            ('m', 'q'): self.calc_m_q,
+            ('po', 'po'): self.calc_po_po,
+            ('po', 'q'): self.calc_po_q
         }
