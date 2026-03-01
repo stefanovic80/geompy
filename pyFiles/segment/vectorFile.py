b68df0d10e4ffaf92ea7a70c705f091fef1ba1cb
stefanovic80 <stefanovicgh@gmail.com>
2026-03-01 20:26:13 +0100

    vector file updated!

--- pyFiles/segment/vectorFile.py
+++ pyFiles/segment/vectorFile.py
@@ -14,3 +14,9 @@ class vector(segment):
         line = self.ax.quiver(X, Y, U, V, angles= 'xy', scale_units='xy', scale=1, width = 0.003)
         self.lines = []
         self.lines.append(line)
+
+    @property
+    def flip(self):
+        self.data[0] = self.data[0][::-1]
+        self.data[1] = self.data[1][::-1]
+        self.onlyDraw()
