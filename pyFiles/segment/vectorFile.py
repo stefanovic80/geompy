c746fc31b97409bcf874788fda0b3576473bb641
stefanovic80 <stefanovicgh@gmail.com>
2026-03-14 11:51:35 +0100

    tip arrow adjusted!

--- pyFiles/segment/vectorFile.py
+++ pyFiles/segment/vectorFile.py
@@ -11,7 +11,19 @@ class vector(segment):
     def onlyDraw(self):
         self.__del__()
         X, Y, U, V = self.data[0][0], self.data[1][0], -self.data[0][0] + self.data[0][1], -self.data[1][0] + self.data[1][1]
-        line = self.ax.quiver(X, Y, U, V, angles= 'xy', scale_units='xy', scale=1, width = 0.003, headwidth=10, headlength=10, headaxislength=9)
+        #line = self.ax.quiver(X, Y, U, V, angles= 'xy', scale_units='xy', scale=1, width = 0.003, headwidth=6, headlength=6, headaxislength=9)
+        
+        line = self.ax.quiver(
+            X, Y, U, V, 
+            angles='xy', 
+            scale_units='xy', 
+            scale=1, 
+            width=0.003, 
+            headwidth=6,      # Larghezza della base della punta
+            headlength=9,     # Lunghezza totale della punta
+            headaxislength=9  # <--- Impostalo UGUALE a headlength per base piatta
+        )
+
         self.lines = []
         self.lines.append(line)
 
