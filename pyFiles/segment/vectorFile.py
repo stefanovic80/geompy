4ca32c66fad4e5b60c279785541fc82a0a1db31d
stefanovic80 <stefanovicgh@gmail.com>
2026-03-20 19:08:35 +0100

    updates!

--- pyFiles/segment/vectorFile.py
+++ pyFiles/segment/vectorFile.py
@@ -8,6 +8,29 @@ from .segmentFile import segment
 
 class vector(segment):
 
+    def onlyDrawFlip(self):
+        self.__del__()
+
+
+        X, Y, U, V = self.data[0][0], self.data[1][0], -self.data[0][0] + self.data[0][1], -self.data[1][0] + self.data[1][1]
+
+        #X, Y, U, V = self.data[0][1], self.data[1][1], -self.data[0][1] + self.data[0][0], -self.data[1][1] + self.data[1][0]
+        line = self.ax.quiver(
+            X, Y, U, V,
+            angles='xy',
+            scale_units='xy',
+            scale=1,
+            width=0.003,
+            headwidth=6,      # Larghezza della base della punta
+            headlength=9,     # Lunghezza totale della punta
+            headaxislength=9,  # <--- Impostalo UGUALE a headlength per base piatta
+            color = self._color
+        )
+
+        self.lines = []
+        self.lines.append(line)
+
+
     def onlyDraw(self):
         self.__del__()
         
@@ -40,4 +63,4 @@ class vector(segment):
     def flip(self):
         self.data[0] = self.data[0][::-1]
         self.data[1] = self.data[1][::-1]
-        self.onlyDraw()
+        self.onlyDrawFlip()
